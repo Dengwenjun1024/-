6e8b070d68544ab4f31760a72a8b72a82988c363
邓文俊 <614513153@qq.com>
2019-06-12 21:58:25 +0800

    事件（Event）的应用

--- Event.py
+++ Event.py
@@ -7,7 +7,11 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 def boss(event:Event):
     logging.info("I'm boss, waiting for U.")
+<<<<<<< HEAD
     # 等待
+=======
+    # 等待，直到flag为True
+>>>>>>> 事件（Event）的应用
     event.wait()
     logging.info("Good job.")
 
@@ -19,7 +23,11 @@ def worker(event:Event, count=10):
         time.sleep(0.5)
         cups.append(1)
         if len(cups) >= count:
+<<<<<<< HEAD
             # 通知
+=======
+            # 通知，将falg设置为True，老板那边就可以继续执行代码了。
+>>>>>>> 事件（Event）的应用
             event.set()
             break
     logging.info("I'm finished my job. cups={}".format(cups))
