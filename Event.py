b7e3f9540fb1198213316284833c1dd7ff012440
邓文俊 <614513153@qq.com>
2019-06-12 22:30:49 +0800

    Event的应用

--- Event.py
+++ Event.py
@@ -7,11 +7,7 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 def boss(event:Event):
     logging.info("I'm boss, waiting for U.")
-<<<<<<< HEAD
     # 等待
-=======
-    # 等待，直到flag为True
->>>>>>> 事件（Event）的应用
     event.wait()
     logging.info("Good job.")
 
@@ -23,11 +19,7 @@ def worker(event:Event, count=10):
         time.sleep(0.5)
         cups.append(1)
         if len(cups) >= count:
-<<<<<<< HEAD
             # 通知
-=======
-            # 通知，将falg设置为True，老板那边就可以继续执行代码了。
->>>>>>> 事件（Event）的应用
             event.set()
             break
     logging.info("I'm finished my job. cups={}".format(cups))
@@ -37,3 +29,18 @@ w = Thread(target=worker, args=(event,))
 b = Thread(target=boss, args=(event,))
 w.start()
 b.start()
+
+# 2019-06-12 22:30:14,285 Thread-1 8796 I'm working for U.
+# 2019-06-12 22:30:14,286 Thread-1 8796 make 1
+# 2019-06-12 22:30:14,286 Thread-2 8516 I'm boss, waiting for U.
+# 2019-06-12 22:30:14,786 Thread-1 8796 make 1
+# 2019-06-12 22:30:15,286 Thread-1 8796 make 1
+# 2019-06-12 22:30:15,787 Thread-1 8796 make 1
+# 2019-06-12 22:30:16,287 Thread-1 8796 make 1
+# 2019-06-12 22:30:16,787 Thread-1 8796 make 1
+# 2019-06-12 22:30:17,287 Thread-1 8796 make 1
+# 2019-06-12 22:30:17,787 Thread-1 8796 make 1
+# 2019-06-12 22:30:18,287 Thread-1 8796 make 1
+# 2019-06-12 22:30:18,787 Thread-1 8796 make 1
+# 2019-06-12 22:30:19,287 Thread-1 8796 I'm finished my job. cups=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# 2019-06-12 22:30:19,287 Thread-2 8516 Good job.
